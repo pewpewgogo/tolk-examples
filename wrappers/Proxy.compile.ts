01564b170c13e0902d7ebad60fa914950cebd783
Vladimir Plotvinov <vplotvinov@Vladimirs-MacBook-Pro.local>
2025-02-03 10:40:02 +0100

    separate by folders

--- wrappers/Proxy.compile.ts
+++ wrappers/Proxy.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tolk',
-    entrypoint: 'contracts/proxy.tolk',
+    entrypoint: 'contracts/proxy/proxy.tolk',
     withStackComments: true,    // Fift output will contain comments, if you wish to debug its output
     experimentalOptions: '',    // you can pass experimental compiler options here
 };
--- wrappers/SimpleCounter.compile.ts
+++ wrappers/SimpleCounter.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tolk',
-    entrypoint: 'contracts/simpleCounter.tolk',
+    entrypoint: 'contracts/simpleCounter/simpleCounter.tolk',
     withStackComments: true,    // Fift output will contain comments, if you wish to debug its output
     experimentalOptions: '',    // you can pass experimental compiler options here
 };
